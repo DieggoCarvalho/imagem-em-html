e04fbbad09fbe88e881429060b9ba4049addd3c2
Dieggo <45542233+DieggoCarvalho@users.noreply.github.com>
2023-05-03 21:24:31 -0300

    add sections & footer

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const d = new Date(); // criar data
+const year = document.getElementById('current-year');// obter o span de id = current-year
+year.innerHTML = d.getFullYear(); // ano completo na span de id = current-year
---
+++ layout/0_site 02 no-response model upgraded/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<title>Model Site</title>
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<!-- <link rel="stylesheet" href="assets/css/reset.css" /> -->
+	<meta charset="UTF-8" />
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="logo">
+				<img src="assets/img/homem.png" alt="logo" />
+				<h1 id="logo">Logomarca</h1>
+			</div>
+			<nav class="menu">
+				<ul>
+					<li><a href="#">HOME</a></li>
+					<li><a href="#">SOBRE</a></li>
+					<li><a href="#">NOTÍCIAS</a></li>
+					<li><a href="#">PRODUTOS</a></li>
+					<li><a href="#">CONTATO</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="banner">
+		<div class="container">
+			<div class="introducao">
+				<div class="frase f1">
+					Frase de EFEITO
+				</div>
+				<div class="frase f2">
+					Outras frases que complementam a Frase <br /> de Efeito.
+				</div>
+				<div class="btn">
+					<a href="#">Acessar o Produto</a>
+				</div>
+			</div>
+			<div class="banner-img">
+				<img src="assets/img/homem.png" alt="banner-img" />
+			</div>
+		</div>
+	</section>
+	<section class="content">
+		<div class="container">
+			<h1>Outros Produtos</h1>
+			<div class="figures">
+				<span id="figure-img1" class="figure">
+					<div><a href="#">Jaquetas</a></div>
+				</span>
+				<span id="figure-img2" class="figure">
+					<div><a href="">Blusas</a></div>
+				</span>
+				<span id="figure-img3" class="figure">
+					<div><a href="">Bermudas</a></div>
+				</span>
+				<span id="figure-img4" class="figure">
+					<div><a href="">Casacos</a></div>
+				</span>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="container">
+			<span class="copy">
+				&copy; Copyright -
+			</span>
+				Model Site
+			<span class="copy">
+				by
+			</span>
+				allbuquerque. Todos os direitos reservados.
+		</div>
+	</footer>
+	
+</body>
+</html>
