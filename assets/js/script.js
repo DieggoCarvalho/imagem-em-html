479eafb3094e62243794fb0dcac618a39bbc20ce
Dieggo <dieggo.dev@outlook.com>
2023-10-19 12:32:19 -0300

    feat (index): responsividade - add media.css - correções gerais - add fontawesome.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 const d = new Date(); // criar data
-const year = document.getElementById('current-year');// obter o span de id = current-year
+const year = document.getElementById("current-year"); // obter o span de id = current-year
 year.innerHTML = d.getFullYear(); // ano completo na span de id = current-year
+
+window.onload = function () {
+  document.querySelector(".menuMobile").addEventListener("click", function () {
+    if (document.querySelector(".menu nav ul").style.display == "flex") {
+      document.querySelector(".menu nav ul").style.display = "none";
+    } else {
+      document.querySelector(".menu nav ul").style.display = "flex";
+    }
+  });
+};
